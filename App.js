56231f246916c345bbcdcaf9b4be7e1a6d828334
Anıl <anil@Anl-MacBook-Air.local>
2023-11-30 20:07:11 +0300

    Custom component

--- App.js
+++ App.js
@@ -1,28 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import racesResponse from './assets/data/races.json';
-import { Entypo } from '@expo/vector-icons';
-import Colors from './src/Constants/Colors';
+import RaceListItem from './src/components/RaceListItem';
 const races = racesResponse.data.races.response;
-
-function RaceListItem({ item }) {
-  return (
-    <View style={styles.itemContainer}>
-      <View style={styles.datesContainer}>
-        <Text style={styles.date}>03-05</Text>
-        <Text style={styles.month}>NOV</Text>
-      </View>
-      <View style={{ flex: 1 }}>
-        <Text style={styles.round}>Round 21</Text>
-        <Text style={styles.country}>{item.competition.location.country}</Text>
-        <Text style={styles.description}>
-          Formula 1 {item.competition.name} 2023
-        </Text>
-      </View>
-      <Entypo name="chevron-right" size={24} color="#ED0500" />
-    </View>
-  );
-}
 
 export default function App() {
   return (
@@ -40,49 +20,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'whitesmoke',
-  },
-  itemContainer: {
-    padding: 10,
-    margin: 5,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.18,
-    shadowRadius: 1.0,
-    elevation: 1,
-  },
-  datesContainer: {
-    padding: 10,
-    marginRight: 10,
-    borderRightWidth: 1,
-    borderColor: 'gainsboro',
-    alignItems: 'center',
-  },
-  date: {},
-  month: {
-    backgroundColor: 'gainsboro',
-    paddingVertical: 3,
-    paddingHorizontal: 10,
-    marginTop: 5,
-    borderRadius: 10,
-    overflow: 'hidden',
-    color: 'dimgrey',
-    fontWeight: 'bold',
-  },
-  round: {
-    color: '#ED0500',
-  },
-  country: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  description: {
-    color: 'dimgrey',
   },
 });
